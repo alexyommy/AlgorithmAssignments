b056fb46feac39224fe5acfab411f39e5e09511f
Alex Yom <alexyom24@gmail.com>
2021-06-26 22:04:55 -0400

    recursion

---
+++ Recursion/toDo1.js
@@ -0,0 +1,2 @@
+// Recurseive Sigma
+// Write a recursive function that given a number returns the sum of integers from 1 to that number Example: rSigma(5) = 15 (1+2+3+1+5); rSigma = 3 (1+2); rSigma(-1)=0
