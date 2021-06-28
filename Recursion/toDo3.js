0961bf166d8c58e33178011bd87bc2a0a035a35f
Alex Yom <alexyom24@gmail.com>
2021-06-27 22:53:47 -0400

    todo3

--- Recursion/toDo3.js
+++ Recursion/toDo3.js
@@ -1,1 +1,39 @@
 // Recursive Binary Search
+function rBinarySearch(arr, value) {
+    var mid = Math.floor(arr.length/2)
+    if(arr[mid] == value) {
+    return true;
+    }
+    else if(value < arr[mid] && arr.length > 1) {
+        return rBinarySearch(arr.slice(0,mid), value);
+    }
+    else if(value > arr[mid] && arr.length > 1) {
+        return rBinarySearch(arr.slice(mid, arr.length), value);
+    } else {
+        return false
+    }
+}
+// console.log(rBinarySearch([1,3,5,6],4));
+// console.log(rBinarySearch([4,5,6,8,12],5));
+
+// Greatest Common Factor
+function rGCF(num1,num2) {
+    if(num1 == num2) {
+        return num1;
+    }
+    if(num1 > num2) {
+        if ((num1/num2) % 1 === 0) {
+            return num2;
+        } else {
+            return rGCF(num1-num2, num2);
+        }
+    }
+    if(num1 < num2) {
+        if ((num1/num2) % 1 === 0) {
+            return num2;
+        } else {
+            return rGCF(num1, num2-num1);
+        }
+    }
+}
+console.log(rGCF(123456, 987654))
